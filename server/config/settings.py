5295767f13f7ab3b9f16505c8fbad80a54949cd7
Ibrahim Abu Eita <ebrahimaboeita990@gmail.com>
2026-04-19 18:03:27 +0200

    arch: Database Connection [Neon Postgress]

--- server/config/settings.py
+++ server/config/settings.py
@@ -1,5 +1,8 @@
 import os
 from pathlib import Path
+
+import dj_database_url
+
 from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -59,10 +62,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
